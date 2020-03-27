42ce023febbb59078aa8cb4b3c2d3074c4095c0a
ali <aliyoussef34@hotmail.fr>
2020-03-27 01:04:48 +0100

    GET '/movies' functionnal, returns a random must watch movie

--- server/routes/movies.js
+++ server/routes/movies.js
@@ -2,12 +2,28 @@ const express = require('express');
 const router = express.Router();
 const imdb = require('../imdb')
 const Movie = require('../models/Movie')
-const DENZEL_IMDB_ID = 'nm0000243';
+//const DENZEL_IMDB_ID = 'nm0000243';
 
+/*
+  Get random must watch movie 
+*/
+router.get('/', async (req, res) => {
+  let movie;
 
-router.get('/', (request, response) => {
-    response.send('Movies');
-  });
+	try {
+		movie = await Movie.aggregate([{ $match: { metascore: { $gt: 77 } } }, { $sample: { size: 3} }]);
+  } 
+  catch (err) {
+		return res.status(500).json(err);
+  }
+  
+  if (movie.length === 0) {
+    return res.status(404).send('No movie found.');
+  } else {
+    console.log(movie.length)
+    res.send(movie[0])
+  }
+});
 
 
 
@@ -18,11 +34,11 @@ router.post('/populate/:id', async (req,res) => {
   const movies = await imdb(req.params.id)
   console.log(`🍿 ${movies.length} movies found.`);
 
-  // for (const m of movies) {
-  //   const movie = new Movie(m)
-  //   const savedMovie = await movie.save();
-  //   console.log(savedMovie)
-  // }
+  for (const m of movies) {
+    const movie = new Movie(m)
+    const savedMovie = await movie.save();
+    console.log(savedMovie)
+  }
 });
 
 
